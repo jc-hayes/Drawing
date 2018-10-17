d83330b0cb2d25aaffb98cdcbd183d4ff406078f
James H <jchayes86@gmail.com>
2018-10-16 21:08:35 -0700

    half-way-complete

--- app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
+++ app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
@@ -1,6 +1,7 @@
 package edu.cascadia.brianb.mydrawing;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -8,10 +9,12 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathDashPathEffect;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.FrameLayout;
 
 /**
  * Edited by Brian Bansenauer on 10/18/15.
@@ -40,24 +43,43 @@ public class SimplyDrawnView extends View {
 
         //Draw line
         mPaint.setColor(Color.BLACK);
-        mPaint.setStrokeWidth(16.0f);
+        mPaint.setStrokeWidth(10);
         canvas.drawLine(mWidth, 0, 200, mHeight, mPaint);
 
         //Draw red line
-        mPaint.setColor(Color.RED); mPaint.setStrokeWidth(10);
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(16.0f);
+        canvas.drawLine(mWidth,mHeight,(mWidth/2),(mHeight/2),mPaint);
 
         //Draw green lines
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStrokeWidth(30.0f);
+        canvas.drawLine(10,20,mWidth,20,mPaint);
+        canvas.drawLine(10,140,mWidth,140,mPaint);
+        canvas.drawLine(10,260,mWidth,260,mPaint);
+        canvas.drawLine(10,380,mWidth,380,mPaint);
+        canvas.drawLine(10,500,mWidth,500,mPaint);
+        canvas.drawLine(10,620,mWidth,620,mPaint);
 
         //Draw Text
+        mPaint.setColor(Color.BLACK);
+        mPaint.setTextSize(80);
+        canvas.drawText("I am a Drawing",10,(mHeight/2),mPaint);
 
-        //Draw Text on a Path
+        //TODO: Draw Text on a Path
 
-        //Draw filled, opaque, and open ovals
+        //TODO: Draw filled, opaque, and open ovals
 
         //Draw bee bitmap
-        Drawable b = getResources().getDrawable(R.drawable.bee,null);
+        FrameLayout myCanvas = (FrameLayout) findViewById(R.id.mycanvas);
 
-        //Add another image to drawable and draw it
+        Resources res = getResources();
+        Bitmap b = BitmapFactory.decodeResource(res,R.drawable.bee);
+
+        canvas.drawBitmap(b,10,10,null);
+
+
+        //TODO: Add another image to drawable and draw it
 
     }
 }
