26502865f50ff6d697ad74c351d047dca4f46cd6
James H <jchayes86@gmail.com>
2018-11-08 12:18:10 -0800

    did to-dos

--- app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
+++ app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
@@ -23,12 +23,19 @@ public class SimplyDrawnView extends View {
 
     private Paint mPaint;
     private Path mPath;
+    private Paint txtPaint;
+    private Paint ovalPaint;
     private float mWidth, mHeight;
+    private Paint lnPaint;
 
     public SimplyDrawnView(Context context, AttributeSet attributeSet){
         super(context, attributeSet);
         mPaint = new Paint();
         mPath = new Path();
+        txtPaint = new Paint();
+        ovalPaint = new Paint();
+        lnPaint = new Paint();
+
      }
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
@@ -44,42 +51,59 @@ public class SimplyDrawnView extends View {
         //Draw line
         mPaint.setColor(Color.BLACK);
         mPaint.setStrokeWidth(10);
+        mPaint.setAlpha(70);
         canvas.drawLine(mWidth, 0, 200, mHeight, mPaint);
 
         //Draw red line
+        mPaint.setAlpha(100);
         mPaint.setColor(Color.RED);
         mPaint.setStrokeWidth(16.0f);
         canvas.drawLine(mWidth,mHeight,(mWidth/2),(mHeight/2),mPaint);
 
         //Draw green lines
-        mPaint.setColor(Color.GREEN);
-        mPaint.setStrokeWidth(30.0f);
-        canvas.drawLine(10,20,mWidth,20,mPaint);
-        canvas.drawLine(10,140,mWidth,140,mPaint);
-        canvas.drawLine(10,260,mWidth,260,mPaint);
-        canvas.drawLine(10,380,mWidth,380,mPaint);
-        canvas.drawLine(10,500,mWidth,500,mPaint);
-        canvas.drawLine(10,620,mWidth,620,mPaint);
+        lnPaint.setColor(Color.GREEN);
+        lnPaint.setStrokeWidth(35.0f);
+        int i = 120;
+
+        for (int n = 5; n > 0; n--) {
+            // set transparent pen
+            lnPaint.setAlpha(100-(n*10));
+            int yCord = ((n-1)*i)+20;
+            canvas.drawLine(10,yCord,mWidth,yCord,lnPaint);
+        }
 
         //Draw Text
         mPaint.setColor(Color.BLACK);
-        mPaint.setTextSize(80);
+        mPaint.setTextSize(65);
         canvas.drawText("I am a Drawing",10,(mHeight/2),mPaint);
 
-        //TODO: Draw Text on a Path
+        //Draw Text on a Path
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        mPath.addCircle((mWidth/2), (mHeight/2), 500, Path.Direction.CW);
+        txtPaint.setTextSize(100);
+        txtPaint.setAlpha(50);
+        canvas.drawTextOnPath("this text is on a circular path, cool", mPath, 0, 0, txtPaint);
 
-        //TODO: Draw filled, opaque, and open ovals
+        //Draw filled, opaque, and open ovals
+        ovalPaint.setColor(Color.BLACK);
+        ovalPaint.setStrokeWidth(20.0f);
+        ovalPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawOval((mWidth/2)-255, (mHeight/2)-255, (mWidth/2)+255, (mHeight/2)+255, ovalPaint);
+
+        ovalPaint.setStyle((Paint.Style.FILL_AND_STROKE));
+        ovalPaint.setColor(Color.MAGENTA);
+        ovalPaint.setAlpha(50);
+        canvas.drawOval((mWidth/2)-250, (mHeight/2)-250, (mWidth/2)+250, (mHeight/2)+250, ovalPaint);
+
+        ovalPaint.setColor(Color.RED);
+        ovalPaint.setAlpha(200);
+        canvas.drawOval((mWidth/2)-25, (mHeight/2)-25, (mWidth/2)+25, (mHeight/2)+25, ovalPaint);
 
         //Draw bee bitmap
-        FrameLayout myCanvas = (FrameLayout) findViewById(R.id.mycanvas);
-
         Resources res = getResources();
         Bitmap b = BitmapFactory.decodeResource(res,R.drawable.bee);
-
         canvas.drawBitmap(b,10,10,null);
 
-
-        //TODO: Add another image to drawable and draw it
 
     }
 }
